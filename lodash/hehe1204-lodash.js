ec74028f66967fddd19cc2e7a85ce0b7c9791f99
hehe1204 <1432046563@qq.com>
2018-07-22 21:13:32 +0800

    second

--- lodash/hehe1204-lodash.js
+++ lodash/hehe1204-lodash.js
@@ -8,4 +8,34 @@ var hehe1204 = {
 			return result
 		}
      ,
-}
+     compoct:function(ary){
+     	var result = []
+     	for(var i = 0;i < ary.length;i++){
+     		if(ary[i]){
+     			result.push(ary[i])
+     		}
+     	return result
+     }
+     ,
+    drop:function(ary,n = 1){
+        var result = []
+        for(var i = n;i < ary.length;i++){
+            result.push(ary[i])
+        }
+
+        return result
+    }
+    ,
+    dropRight(ary,n = 1){
+        var result = []
+        for(var i = ary.length - (n + 1);i >= 0;i--){
+        result.unshift(ary[i])
+        }
+
+        return result
+    }
+    ,
+
+
+    
+}    
